7d970e177fdb41d579c3cbe7168277290462cdd5
callas4ever <thetseringnorbu@gmail.com>
2020-07-13 17:41:59 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import ImageSlider from './ImageSlider';
 import Bomb from './Bomb';
 
 ReactDOM.render(
+   
   <div>
-    <ImageSlider />
+    <ImageSlider  />
     <Bomb initialCount={120} />
   </div>,
   document.getElementById('root')
